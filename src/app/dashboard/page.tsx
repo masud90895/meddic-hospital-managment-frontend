f88b6e3f69923aea1ef19cabe3bffd4bd91b3eb5
masud90895 <masudhossainmbs129@gmail.com>
2023-10-19 22:56:31 +0600

    [complete] alhamdulillah

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -63,10 +63,36 @@ const Dashboard = () => {
         )}
       </div>
     );
-    {
-      /* END Statistics: Simple with Action */
-    }
+  } else if (
+    user?.role === USER_ROLE.ADMIN ||
+    user?.role === USER_ROLE.SUPER_ADMIN
+  ) {
+    dashboardData = (
+      <div>
+        {isLoading ? (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 lg:gap-8">
+            <Skeleton />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 lg:gap-8">
+            <div className="flex flex-col rounded-lg shadow-sm bg-white overflow-hidden dark:text-gray-100 dark:bg-gray-800">
+              {/* Body */}
+              <div className="p-5 grow flex justify-between items-center">
+                <dl className="space-y-1">
+                  <dt className="text-2xl font-bold">{data}</dt>
+                  <dd className="uppercase font-semibold text-sm text-gray-500 tracking-wider dark:text-gray-400">
+                    User all appointment
+                  </dd>
+                </dl>
+              </div>
+              {/* END Body */}
+            </div>
+          </div>
+        )}
+      </div>
+    );
   }
+
   return <>{dashboardData}</>;
 };
 
